844d9b7bb3c43846ba7a7bfbded27e7e4ecf5e97
Pablo Radomski <pablo.radomski@tiendanube.com.ar>
2020-04-16 11:17:13 -0300

    feat (*): Added filter to WrapGrid

--- src/utils/WrapGrid/WrapGrid.js
+++ src/utils/WrapGrid/WrapGrid.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./WrapGrid.css";
 import Icon from "../../components/Icon/Icon";
@@ -7,17 +7,38 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 
 const WrapGrid = (props) => {
   const Icons = { ...fal, ...fab };
+  const [filter, setFilter] = useState("");
   return (
-    <div {...props} className={`wrapgrid ${props.className}`}>
-      {Object.keys(Icons).map((i) => {
-        const internalIconName = i.substring(2);
-        return (
-          <div className="wrapgrid_icon-block">
-            <h3>{internalIconName}</h3>
-            <Icon icon={internalIconName} size="2x" />
-          </div>
-        );
-      })}
+    <div class="wrapgrid">
+      <div>
+        <input
+          type="text"
+          class="filter"
+          value={filter}
+          placeholder="Buscador"
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+        />
+      </div>
+      <div {...props} className={`grid ${props.className}`}>
+        {Object.keys(Icons)
+          .filter((i) =>
+            i
+              .substring(2)
+              .toLowerCase()
+              .startsWith(filter.toLowerCase())
+          )
+          .map((i) => {
+            const internalIconName = i.substring(2);
+            return (
+              <div className="wrapgrid_icon-block" key={i}>
+                <h3>{internalIconName}</h3>
+                <Icon icon={i.substring(2)} size="2x" />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
