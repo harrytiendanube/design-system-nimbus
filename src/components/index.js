ccb882ee535efd62526291221b368bdeac1177f4
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-04-20 18:34:08 -0300

    feat(*): created new component Link

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Button from "./Button";
 import Icon, { icons, variantIcons } from "./Icon";
+import Link from "./Link";
 
-export { Button, Icon, icons, variantIcons };
+export { Button, Icon, icons, variantIcons, Link };
