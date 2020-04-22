8f6d3d1c7e2f8643df488298f606b8159589e32e
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-04-22 16:50:48 -0300

    feat(*): Added new text as component.

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Icon, { icons, variantIcons } from "./Icon";
 import Link from "./Link";
 import Input from "./Input";
+import Text from "./Text";
 
-export { Button, Icon, icons, variantIcons, Link, Input };
+export { Button, Icon, icons, variantIcons, Link, Input, Text };
