6660287dd56eeeb8f72a619e0908f5e2e236484c
kelman celis <kelman.celis@tiendanube.com>
2020-04-29 22:31:57 -0300

    chore(storybook) applying custom logo and url

---
+++ packages/storybook/.storybook/storyTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Design system tiendanube/nimbus',
+  brandUrl: 'https://www.tiendanube.com/',
+  brandImage:
+    'https://d26lpennugtm8s.cloudfront.net/assets/insti/img/json-ld-tiendanube.png',
+});
