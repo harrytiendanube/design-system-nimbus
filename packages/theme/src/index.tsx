3f5cf379bbf0d0fe7331afb7c2315bde4ffb3b0b
kelman celis <kelman.celis@tiendanube.com>
2020-04-29 21:42:02 -0300

    chore(theme) added sass and created globals sass file to POC improvements eslint

--- packages/theme/src/index.tsx
+++ packages/theme/src/index.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import styled from 'styled-components'
+
+const variables = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!../src/vars.scss')
+
 const Theme: Record<string, Record<string, string>> = {
   colors: {
-    primary: 'aliceblue'
+    primary: 'blue'
   }
 }
+
+export { styled, variables }
 
 export default Theme
---
+++ packages/theme/src/smole.test.ts
@@ -0,0 +1,5 @@
+describe('Smoke test theme', () => {
+  it('Should be return sum ', () => {
+    expect(2).toBe(2)
+  })
+})
