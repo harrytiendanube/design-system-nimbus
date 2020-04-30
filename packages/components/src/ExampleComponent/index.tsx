33ded31415412832265d7e82467a799dbbadb67b
kelman celis <kelman.celis@tiendanube.com>
2020-04-29 21:54:22 -0300

    chore(components) use styles theme example

--- packages/components/src/ExampleComponent/index.tsx
+++ packages/components/src/ExampleComponent/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
-import styles from './styles.module.css';
+
+import { styled, variables } from '@tiendanube/theme';
+
+const ExampleComponentStyled = styled.div`
+  margin: 2em;
+  padding: 0.5em;
+  border: 2px solid red;
+  font-size: 2em;
+  text-align: center;
+  background-color: ${variables.secondary};}
+`;
 
 interface Props {
   text: string;
 }
 
 const ExampleComponent: React.FC<Props> = ({ text }) => {
-  return <div className={styles.test}>Example Component : {text}</div>;
+  return (
+    <ExampleComponentStyled>Example Component : {text}</ExampleComponentStyled>
+  );
 };
 
 export default ExampleComponent;
