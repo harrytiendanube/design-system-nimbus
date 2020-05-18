ad169220ec0004ecb604bbdf884d1f3b6e0bc266
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-05-18 13:29:44 -0300

    feat(*): Added title component.

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -3,3 +3,7 @@ import Icon, { variantIcons } from "./Icon";
 import Input from "./Input";
 import Link from "./Link";
 export { Button, Icon, variantIcons, Input, Link };
+
+// Title reference added
+export { default as Title } from "./Title";
+export * from "./Title";
