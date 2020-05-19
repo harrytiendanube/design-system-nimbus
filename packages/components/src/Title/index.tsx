6cab72fddf9c66900237c80ab3c06d5128f96913
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-05-19 00:13:03 -0300

    wip(*): Added styles to each type heading.

--- packages/components/src/Title/index.tsx
+++ packages/components/src/Title/index.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
 
 interface Props
-  extends Omit<
-    React.HTMLAttributes<HTMLHeadingElement>,
-    "className" | "children" | "style"
-  > {
+  extends Omit<React.HTMLAttributes<HTMLHeadingElement>, "children" | "style"> {
   /** Element inside tag component */
   children: React.ReactNode;
   /** Heading type */
   type?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
-const Title: React.FC<Props> = ({ children, type, ...share }: Props) => {
+const Title: React.FC<Props> = ({ children, type = "h1", ...share }: Props) => {
   const HeadTag = `${type}`;
-  return <HeadTag {...share}>{children}</HeadTag>;
+
+  // TODO: @Juan falta definir estas clases.
+  const classesTypes = {
+    h1: "main-title",
+    h2: "second-title",
+    h3: "t-title",
+    h4: "t-title",
+  };
+  const props = Object.assign({}, share, { className: classesTypes[type] });
+  return <HeadTag {...props}>{children}</HeadTag>;
 };
 
 Title.defaultProps = {
