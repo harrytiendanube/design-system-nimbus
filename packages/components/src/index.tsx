91d2cbc4fa625b400bb88c80ecbc7ad75f3f99e7
Pablo Radomski <pablo.radomski@tiendanube.com.ar>
2020-05-22 15:42:37 -0300

    feat(Header) New Header component created

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -5,3 +5,7 @@ import Link from "./Link";
 import Text from "./Text";
 import Title from "./Title";
 export { Button, Icon, variantIcons, Input, Link, Text, Title };
+
+// Header reference added
+export { default as Header } from "./Header";
+export * from "./Header";
