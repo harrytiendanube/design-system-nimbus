0bf2033ff6e98a8a07e184aa1679ae199d7ee4e0
Pablo Radomski <pablo.radomski@tiendanube.com.ar>
2020-05-27 21:38:47 -0300

    feat(alert) - format code in test file

--- packages/components/src/Alert/Alert.test.js
+++ packages/components/src/Alert/Alert.test.js
@@ -5,9 +5,7 @@ import "@testing-library/jest-dom/extend-expect";
 
 describe("Alert", () => {
   it("Render Alert", () => {
-    const { getByTestId } = render(
-      <Alert data-testid="Alert">children</Alert>
-    );
+    const { getByTestId } = render(<Alert data-testid="Alert">children</Alert>);
     const component = getByTestId("Alert");
     /** Validate than text is the same as the content tag */
     expect(component).toHaveTextContent("children");
