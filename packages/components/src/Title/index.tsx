88f13d8cf4d9a6056d71b352635954e791ecf58e
kelman celis <kelman.celis@tiendanube.com>
2020-05-29 09:53:12 -0300

    chore(review) solved br in react import and remove blank comment

--- packages/components/src/Title/index.tsx
+++ packages/components/src/Title/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import "@tiendanube/styles/css/Title.css";
 
 export interface InterfaceTitle
@@ -9,7 +10,7 @@ export interface InterfaceTitle
   /**
    * Heading type of html heading
    */
-  children: React.ReactChildren;
+  children: React.ReactText;
   /**
    * Element inside tag component
    */
@@ -17,7 +18,6 @@ export interface InterfaceTitle
 }
 
 /**
- *
  * @param children - Element inside tag component
  * @param type - Heading type of html heading
  */
@@ -29,7 +29,7 @@ function Title({
   const HeadTag = `${type}`;
 
   const classesTypes = {
-    // TODO: Reemplazar los valores 01, 02, 03 por los h1, h2, h3, etc.
+    // TODO: replace values 01, 02, 03 to  h1, h2, h3, etc.
     h1: "nimbus--title-01",
     h2: "nimbus--title-02",
     h3: "nimbus--title-03",
@@ -40,9 +40,5 @@ function Title({
   const props = Object.assign({}, share, { className: classesTypes[type] });
   return <HeadTag {...props}>{children}</HeadTag>;
 }
-
-Title.defaultProps = {
-  type: "h1",
-};
 
 export default Title;
