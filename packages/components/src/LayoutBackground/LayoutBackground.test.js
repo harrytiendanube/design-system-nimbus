2a570eb836fb5bf5bfef1cda96876e8a3e00c54a
Pablo Radomski <pablo.radomski@tiendanube.com.ar>
2020-06-02 12:11:31 -0300

    feat(LayoutBackground) - improve test

--- packages/components/src/LayoutBackground/LayoutBackground.test.js
+++ packages/components/src/LayoutBackground/LayoutBackground.test.js
@@ -1,15 +1,46 @@
 import React from "react";
+
+import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import LayoutBackground from ".";
-import "@testing-library/jest-dom/extend-expect";
+import image from "../../../storybook/stories/utils";
 
 describe("LayoutBackground", () => {
-  it("Render LayoutBackground", () => {
+  let myText, classNameBase;
+  const myTestId = "LayoutBackground";
+
+  beforeEach(() => {
+    myText = "myText";
+    classNameBase = "nimbus--layout-background";
+  });
+
+  it("Render with import image", () => {
     const { getByTestId } = render(
-      <LayoutBackground>children</LayoutBackground>,
+      <LayoutBackground image={image}>{myText}</LayoutBackground>,
     );
-    const component = getByTestId("LayoutBackground");
-    /** Validate than text is the same as the content tag */
-    expect(component).toHaveTextContent("children");
+    const component = getByTestId(myTestId);
+    expect(component).toBeTruthy();
+    expect(component).toHaveTextContent(myText);
+    expect(component).toHaveClass(`${classNameBase}`);
+    expect(component).toContainHTML(`<div class="${classNameBase}__content">`);
+    expect(component).toContainHTML(
+      `<div class="${classNameBase}__image" style="background-image: url(${image});"`,
+    );
+  });
+
+  it("Render with URL image", () => {
+    const url =
+      "https://d26lpennugtm8s.cloudfront.net/assets/insti/img/login/login-background.jpg";
+    const { getByTestId } = render(
+      <LayoutBackground image={url}>{myText}</LayoutBackground>,
+    );
+    const component = getByTestId(myTestId);
+    expect(component).toBeTruthy();
+    expect(component).toHaveTextContent(myText);
+    expect(component).toHaveClass(`${classNameBase}`);
+    expect(component).toContainHTML(`<div class="${classNameBase}__content">`);
+    expect(component).toContainHTML(
+      `<div class="${classNameBase}__image" style="background-image: url(${url});"`,
+    );
   });
 });
