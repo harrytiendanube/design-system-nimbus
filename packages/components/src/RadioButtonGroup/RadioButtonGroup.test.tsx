0f839765b5154bc0ec4d2af86e4eb535a6a3447f
Pablo Radomski (personal account) <33379686+radomskip@users.noreply.github.com>
2020-09-11 17:14:52 -0300

    feat(RadioButtonGroup) - added test cases (#162)
    Co-authored-by: Pablo Radomski <pablo.radomski@tiendanube.com.ar>

--- packages/components/src/RadioButtonGroup/RadioButtonGroup.test.tsx
+++ packages/components/src/RadioButtonGroup/RadioButtonGroup.test.tsx
@@ -1,26 +1,87 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import RadioButtonGroup from ".";
+import {
+  InterfaceNameValue,
+  InterfaceRadioButtonOption,
+} from "../common/interfaces";
 
-const options = [
-  { label: "Label 1", value: "Value 1" },
-  { label: "Label 2", value: "Value 2" },
-  { label: "Label 3", value: "Value 3" },
+const myLabelGroup = "myLabelGroup";
+const myName = "myName";
+
+const myOpt: InterfaceRadioButtonOption[] = [
+  { label: "myLabel 1", value: "myValue 1" },
+  { label: "myLabel 2", value: "myValue 2", disabled: false },
+  { label: "myLabel 3", value: "myValue 3", disabled: true },
 ];
 
-describe("Render RadioButtonGroup", () => {
-  it("shows shows single RadioButtonGroup", () => {
+describe("<RadioButtonGroup />", () => {
+  it("render", () => {
     render(
       <RadioButtonGroup
-        label="Radio Button Group Label"
-        name="NameGroup"
-        options={options}
+        label={myLabelGroup}
+        name={myName}
+        value={myOpt[0].value}
+        options={myOpt}
+      />,
+    );
+    const e1: HTMLElement = screen.getByRole("radio", { name: myOpt[0].label });
+    const e2: HTMLElement = screen.getByRole("radio", { name: myOpt[1].label });
+    const e3: HTMLElement = screen.getByRole("radio", { name: myOpt[2].label });
+    expect(e1).toHaveProperty("disabled", false);
+    expect(e1).toHaveProperty("checked", true);
+    expect(e2).toHaveProperty("disabled", false);
+    expect(e2).toHaveProperty("checked", false);
+    expect(e3).toHaveProperty("disabled", true);
+    expect(e3).toHaveProperty("checked", false);
+    expect(screen.getByText(myLabelGroup)).toBeTruthy();
+  });
+
+  it("render without label nor value", () => {
+    render(<RadioButtonGroup name={myName} options={myOpt} />);
+    myOpt.forEach((opt) =>
+      expect(screen.getByRole("radio", { name: opt.label })).toHaveProperty(
+        "checked",
+        false,
+      ),
+    );
+    expect(screen.queryByText(myLabelGroup)).toBeFalsy();
+  });
+
+  it("calls onChange", () => {
+    const handleChange = (event: InterfaceNameValue) => {
+      expect(myOpt[1].value).toContain(event.value);
+    };
+    render(
+      <RadioButtonGroup
+        label={myLabelGroup}
+        name={myName}
+        value={myOpt[0].value}
+        options={myOpt}
+        onChange={handleChange}
+      />,
+    );
+
+    const element: HTMLElement = screen.getByRole("radio", {
+      name: myOpt[1].label,
+    });
+    userEvent.click(element);
+  });
+
+  it("does not calls onChange if it not provided", () => {
+    render(
+      <RadioButtonGroup
+        label={myLabelGroup}
+        name={myName}
+        value={myOpt[0].value}
+        options={myOpt}
       />,
     );
     const element: HTMLElement = screen.getByRole("radio", {
-      name: options[0].label,
+      name: myOpt[1].label,
     });
-    expect(element).toBeTruthy();
+    userEvent.click(element);
   });
 });
