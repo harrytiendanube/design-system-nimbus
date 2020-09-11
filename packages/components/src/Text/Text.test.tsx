7341d47cf8f69ace34adf6721a4de1d87acffa59
Pablo Radomski (personal account) <33379686+radomskip@users.noreply.github.com>
2020-09-11 17:14:23 -0300

    feat(Text) - added test cases (#163)
    Co-authored-by: Pablo Radomski <pablo.radomski@tiendanube.com.ar>

--- packages/components/src/Text/Text.test.tsx
+++ packages/components/src/Text/Text.test.tsx
@@ -3,12 +3,17 @@ import { render, screen } from "@testing-library/react";
 
 import Text from ".";
 
-const pattern = "myText";
+const myText = "myText";
 
-describe("Render Text", () => {
-  it("shows myText message", () => {
-    render(<Text>{pattern}</Text>);
-    const element: HTMLElement = screen.getByText(pattern);
-    expect(element).toBeTruthy();
+describe("<Text />", () => {
+  it("render", () => {
+    render(<Text size="regular">{myText}</Text>);
+    const element: HTMLElement = screen.getByText(myText);
+    expect(element).toHaveClass("nimbus--text nimbus--text_regular");
+  });
+  it("render without size", () => {
+    render(<Text>{myText}</Text>);
+    const element: HTMLElement = screen.getByText(myText);
+    expect(element).toHaveClass("nimbus--text nimbus--text_regular");
   });
 });
