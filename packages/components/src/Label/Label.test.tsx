0b50e3eb467870ca0670af00af479c1a764a4a51
Pablo Radomski (personal account) <33379686+radomskip@users.noreply.github.com>
2020-09-11 17:17:47 -0300

    feat(Label) - added test cases (#160)
    Co-authored-by: Pablo Radomski <pablo.radomski@tiendanube.com.ar>

--- packages/components/src/Label/Label.test.tsx
+++ packages/components/src/Label/Label.test.tsx
@@ -1,27 +1,57 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { CreditCardIcon } from "@tiendanube/icons";
 import Label from ".";
 
-const label = "label";
+const myId = "myId";
+const myLabel = "myLabel";
+const myAppearance = "default";
 
-describe("Render Label", () => {
-  it("shows single Label without OnClick", () => {
-    render(<Label id="id-1" icon={CreditCardIcon} label={label} />);
-    const element: HTMLElement = screen.getByRole("status");
-    expect(element).toBeTruthy();
-  });
-  it("shows single Label with OnClick", () => {
-    render(
+describe("<Label />", () => {
+  it("render as a span (without onClick)", () => {
+    const { container } = render(
       <Label
-        id="id-1"
+        id={myId}
         icon={CreditCardIcon}
-        label={label}
-        onClick={() => null}
+        label={myLabel}
+        appearance={myAppearance}
       />,
     );
-    const element: HTMLElement = screen.getByRole("button", { name: label });
-    expect(element).toBeTruthy();
+    expect(screen.getByRole("status")).toBeTruthy();
+    expect(screen.getByText(myLabel)).toBeTruthy();
+    expect(container.querySelector("svg")).toBeTruthy();
+  });
+
+  it("render as a span (without icon, appearance, onClick)", () => {
+    const myClass = "nimbus--label nimbus--label--default";
+    const { container } = render(<Label id={myId} label={myLabel} />);
+    expect(screen.getByRole("status")).toHaveClass(myClass);
+    expect(screen.getByText(myLabel)).toBeTruthy();
+    expect(container.querySelector("svg")).toBeFalsy();
+  });
+
+  it("render as a Button (with onClick)", () => {
+    const handleClick = jest.fn();
+    const { container } = render(
+      <Label
+        id={myId}
+        icon={CreditCardIcon}
+        label={myLabel}
+        onClick={handleClick}
+        appearance={myAppearance}
+      />,
+    );
+    expect(screen.getByRole("button", { name: myLabel })).toBeTruthy();
+    expect(container.querySelector("svg")).toBeTruthy();
+  });
+
+  it("calls onClick", () => {
+    const handleClick = jest.fn();
+    render(<Label id={myId} label={myLabel} onClick={handleClick} />);
+    const element: HTMLElement = screen.getByRole("button", { name: myLabel });
+    userEvent.click(element);
+    expect(handleClick).toHaveBeenCalled();
   });
 });
