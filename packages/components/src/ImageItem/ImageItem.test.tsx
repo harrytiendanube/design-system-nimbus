b167d0be344c71c18e04aa5a9fcefa97924e6e13
Pablo Radomski (personal account) <33379686+radomskip@users.noreply.github.com>
2020-09-11 17:12:01 -0300

    feat(ImageItem) - added test cases (#156)
    Co-authored-by: Pablo Radomski <pablo.radomski@tiendanube.com.ar>

--- packages/components/src/ImageItem/ImageItem.test.tsx
+++ packages/components/src/ImageItem/ImageItem.test.tsx
@@ -3,18 +3,84 @@ import { render, screen } from "@testing-library/react";
 
 import ImageItem from ".";
 
-describe("Render ImageItem", () => {
-  it("shows single ImageItem", () => {
+const myThumbnail = "https://www.myThumbnail.com/";
+const myLink = "myLink";
+const myLinkTo = "https://www.myLinkTo.com/";
+const mySubtitle = "mySubtitle";
+const myDescription = "myDescription";
+
+describe("<ImageItem />", () => {
+  it("render", () => {
     render(
       <ImageItem
-        thumbnail="https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=943&q=80"
-        link="Product"
-        linkTo="https://www.tiendanube.com/"
-        subtitle="Variant"
-        description="1 x $3.500"
+        thumbnail={myThumbnail}
+        link={myLink}
+        linkTo={myLinkTo}
+        subtitle={mySubtitle}
+        description={myDescription}
       />,
     );
-    const element: HTMLElement = screen.getByRole("listitem");
-    expect(element).toBeTruthy();
+    expect(screen.getByRole("listitem")).toBeTruthy();
+    expect(screen.getByAltText(myLink)).toHaveAttribute("src", myThumbnail);
+    expect(screen.getByText(mySubtitle)).toBeTruthy();
+    expect(screen.getByText(myDescription)).toBeTruthy();
+    expect(screen.getByRole("link", { name: myLink })).toHaveAttribute(
+      "href",
+      myLinkTo,
+    );
+  });
+  it("render without thumbnail", () => {
+    render(
+      <ImageItem
+        link={myLink}
+        linkTo={myLinkTo}
+        subtitle={mySubtitle}
+        description={myDescription}
+      />,
+    );
+    expect(screen.getByRole("listitem")).toBeTruthy();
+    expect(screen.queryByAltText(myLink)).toBeFalsy();
+    expect(screen.getByText(mySubtitle)).toBeTruthy();
+    expect(screen.getByText(myDescription)).toBeTruthy();
+    expect(screen.getByRole("link", { name: myLink })).toHaveAttribute(
+      "href",
+      myLinkTo,
+    );
+  });
+  it("render without subtitle", () => {
+    render(
+      <ImageItem
+        thumbnail={myThumbnail}
+        link={myLink}
+        linkTo={myLinkTo}
+        description={myDescription}
+      />,
+    );
+    expect(screen.getByRole("listitem")).toBeTruthy();
+    expect(screen.getByAltText(myLink)).toHaveAttribute("src", myThumbnail);
+    expect(screen.queryByText(mySubtitle)).toBeFalsy();
+    expect(screen.getByText(myDescription)).toBeTruthy();
+    expect(screen.getByRole("link", { name: myLink })).toHaveAttribute(
+      "href",
+      myLinkTo,
+    );
+  });
+  it("render without description", () => {
+    render(
+      <ImageItem
+        thumbnail={myThumbnail}
+        link={myLink}
+        linkTo={myLinkTo}
+        subtitle={mySubtitle}
+      />,
+    );
+    expect(screen.getByRole("listitem")).toBeTruthy();
+    expect(screen.getByAltText(myLink)).toHaveAttribute("src", myThumbnail);
+    expect(screen.getByText(mySubtitle)).toBeTruthy();
+    expect(screen.queryByText(myDescription)).toBeFalsy();
+    expect(screen.getByRole("link", { name: myLink })).toHaveAttribute(
+      "href",
+      myLinkTo,
+    );
   });
 });
