51bac25e0d43d81606256d2a76c226eb210e1f05
Pablo Radomski (personal account) <33379686+radomskip@users.noreply.github.com>
2020-09-11 17:18:10 -0300

    feat(EmpyState) - added test cases (#159)
    Co-authored-by: Pablo Radomski <pablo.radomski@tiendanube.com.ar>

--- packages/components/src/EmptyState/EmptyState.test.tsx
+++ packages/components/src/EmptyState/EmptyState.test.tsx
@@ -1,25 +1,70 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { EmptyState, Text } from "..";
 
-const title = "Title text";
+const myTitle = "myTitle";
+const myText = "myText";
+const myImage = "https://www.myImage.com/";
+const myFullWidth = true;
+const myPAL = "myPAL";
+const mySAL = "mySAL";
 
-describe("Render EmptyState", () => {
-  it("shows single EmptyState", () => {
+describe("<EmptyState />", () => {
+  it("render", () => {
     render(
       <EmptyState
-        fullWidth
-        image="https://d26lpennugtm8s.cloudfront.net/assets/common/img/empty-screens/empty-products.png"
-        title={title}
+        fullWidth={myFullWidth}
+        image={myImage}
+        title={myTitle}
+        primaryActionLabel={myPAL}
+        secondaryActionLabel={mySAL}
       >
-        <Text>Empty state text</Text>
+        <Text>{myText}</Text>
       </EmptyState>,
     );
 
-    const element: HTMLElement = screen.getByRole("heading", {
-      name: title,
-    });
-    expect(element).toBeTruthy();
+    expect(screen.getByRole("heading", { name: myTitle })).toBeTruthy();
+    expect(screen.getByRole("img", { name: myTitle })).toHaveAttribute(
+      "src",
+      myImage,
+    );
+    expect(screen.getByText(myText)).toBeTruthy();
+    expect(screen.getByRole("button", { name: myPAL })).toBeTruthy();
+    expect(screen.getByRole("button", { name: mySAL })).toBeTruthy();
+  });
+  it("render without fullWidth, title, primaryActionLabel, secondaryActionLabel", () => {
+    render(
+      <EmptyState image={myImage}>
+        <Text>{myText}</Text>
+      </EmptyState>,
+    );
+
+    expect(screen.queryByRole("heading", { name: myTitle })).toBeFalsy();
+    expect(screen.queryByRole("img")).toHaveAttribute("src", myImage);
+    expect(screen.queryByRole("button", { name: myPAL })).toBeFalsy();
+    expect(screen.queryByRole("button", { name: mySAL })).toBeFalsy();
+  });
+  it("calls onClickPrimary and onClickSecondary", () => {
+    const handleClickPrimary = jest.fn();
+    const handleClickSecondary = jest.fn();
+    render(
+      <EmptyState
+        fullWidth={myFullWidth}
+        image={myImage}
+        title={myTitle}
+        primaryActionLabel={myPAL}
+        onClickPrimary={handleClickPrimary}
+        secondaryActionLabel={mySAL}
+        onClickSecondary={handleClickSecondary}
+      >
+        <Text>{myText}</Text>
+      </EmptyState>,
+    );
+    userEvent.click(screen.getByRole("button", { name: myPAL }));
+    userEvent.click(screen.getByRole("button", { name: mySAL }));
+    expect(handleClickPrimary).toHaveBeenCalled();
+    expect(handleClickSecondary).toHaveBeenCalled();
   });
 });
