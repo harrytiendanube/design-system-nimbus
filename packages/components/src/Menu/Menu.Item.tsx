7c420e3fa3c62158e23884f51ff13e51f6db30a6
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2021-04-21 12:30:27 -0300

    feat(menu): Add label to menu item and subitem (#325)

--- packages/components/src/Menu/Menu.Item.tsx
+++ packages/components/src/Menu/Menu.Item.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import classNames from "classnames";
 import { Icon as IconType } from "@tiendanube/icons";
+import Label, { InterfaceLabel } from "../Label";
 
 export interface InterfaceMenuItem {
   /** React node of type children */
@@ -13,6 +14,8 @@ export interface InterfaceMenuItem {
   onClick: () => void;
   /** Subsections inside Menu item */
   subItem?: InterfaceMenuSubItem[];
+  /** Menu Item label */
+  label?: Pick<InterfaceLabel, "id" | "appearance" | "icon" | "label">;
 }
 
 export interface InterfaceMenuSubItem {
@@ -22,6 +25,8 @@ export interface InterfaceMenuSubItem {
   onClick: () => void;
   /** Current option selected */
   active?: boolean;
+  /** Menu Item label */
+  label?: Pick<InterfaceLabel, "id" | "appearance" | "icon" | "label">;
 }
 
 function Item({
@@ -30,15 +35,25 @@ function Item({
   active = false,
   onClick,
   subItem,
+  label,
 }: InterfaceMenuItem): JSX.Element {
   const wrapperClass = classNames("nimbus--menu-item-wrapper", {
     "nimbus--menu-item--active": active,
   });
+  const renderLabel = label && (
+    <Label
+      id={label.id}
+      appearance={label.appearance}
+      icon={label.icon}
+      label={label.label}
+    />
+  );
   return (
     <div className={wrapperClass}>
       <button type="button" onClick={onClick} className="nimbus--menu-item">
         <Icon />
-        {children}
+        <span className="nimbus--menu-item__label">{children}</span>
+        {renderLabel}
       </button>
       {subItem && (
         <div className="nimbus--menu-sub-item-wrapper">
@@ -46,6 +61,14 @@ function Item({
             const subItemClass = classNames("nimbus--menu-sub-item", {
               "nimbus--menu-sub-item--active": item.active,
             });
+            const renderSubItemLabel = item.label && (
+              <Label
+                id={item.label.id}
+                appearance={item.label.appearance}
+                icon={item.label.icon}
+                label={item.label.label}
+              />
+            );
             return (
               <button
                 key={index}
@@ -53,7 +76,10 @@ function Item({
                 onClick={item.onClick}
                 className={subItemClass}
               >
-                {item.children}
+                <span className="nimbus--menu-item__label">
+                  {item.children}
+                </span>
+                {renderSubItemLabel}
               </button>
             );
           })}
